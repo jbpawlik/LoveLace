e7659004da94fadfdd7fa70bd41f9f4530fe99c6
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-02-11 14:38:12 -0800

    refactor imports

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { Provider } from 'react-native-paper'
 import { theme } from './src/core/theme'
 import StackNavigator from './src/Components/StackNavigator'
