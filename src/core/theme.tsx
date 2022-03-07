39d7725867f13181b17104952911e988ab71fe9d
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-03-07 08:52:32 -0800

    update theme

--- src/core/theme.tsx
+++ src/core/theme.tsx
@@ -1,16 +1,30 @@
 import { DefaultTheme } from 'react-native-paper'
 
+// Might need to use custom stuff in TypeScript
+// declare global {
+//   namespace ReactNativePaper {
+//     interface ThemeColors {
+//       myOwnColor: string;
+//     }
+
+//     interface Theme {
+//       myOwnProperty: boolean;
+//     }
+//   }
+// }
+
 export const theme = {
   ...DefaultTheme,
+  roundness: 30,  
   colors: {
     ...DefaultTheme.colors,
-    text: '#000000',
-    primary: '#560CCE',
-    secondary: '#414757',
+    text: 'black',
+    primary: 'red',
+    secondary: '#f5f5dc',
     error: '#f13a59',
   },
-  flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  // flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
 }
