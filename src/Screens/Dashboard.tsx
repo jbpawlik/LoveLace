9d5d45930e1a2277f55ee8bcc7aee2c1e7b1e52b
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2022-03-07 09:06:03 -0800

    remove boilerplate

--- src/Screens/Dashboard.tsx
+++ src/Screens/Dashboard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Background from '../Components/Background'
-import Logo from '../Components/Logo'
-import Header from '../Components/Header'
-import Paragraph from '../Components/Paragraph'
-import Button from '../Components/Button'
-import { RootStackParamList } from '../Components/RootStackParamList';
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Paragraph from '../components/Paragraph'
+import Button from '../components/Button'
+import { RootStackParamList } from '../components/RootStackParamList';
 
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
@@ -15,23 +15,6 @@ export default function Dashboard({ navigation }: Props) {
   return (
     <Background>
       <Logo />
-      {/* <Header>Let’s start</Header>
-      <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
-      </Paragraph> */}
-      {/* <Button
-        mode="outlined"
-        style={{flex: 1}}
-        onPress={() =>
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'StartScreen' }],
-          })
-        }
-      >
-        Logout
-      </Button> */}
     </Background>
   )
 }
